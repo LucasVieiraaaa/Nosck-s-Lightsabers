5c058a52f6665995e5f18207f0629d0cb623a43a
LucasVieiraaaa <lucasvieirasouzaa@gmail.com>
2026-02-21 19:30:53 -0300

    update name

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -33,7 +33,7 @@ import org.slf4j.Logger;
 public class ExampleMod
 {
     // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "Noscks Lightsabers";
+    public static final String MOD_ID = "nosckslightsaber";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
