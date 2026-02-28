99f7df975493c7ddbfb80a81d017500f7a56d0fe
LucasVieiraaaa <lucasvieirasouzaa@gmail.com>
2026-02-28 14:33:44 -0300

    Adding the first item of the mod

--- src/main/java/net/nosck/nosckslightsabersmod/NoscksLightsabersMod.java
+++ src/main/java/net/nosck/nosckslightsabersmod/NoscksLightsabersMod.java
@@ -1,6 +1,7 @@
 package net.nosck.nosckslightsabersmod;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -8,10 +9,11 @@ import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.nosck.nosckslightsabersmod.item.ModCreativeModTabs;
+import net.nosck.nosckslightsabersmod.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -19,13 +21,17 @@ import org.slf4j.Logger;
 public class NoscksLightsabersMod
 {
     // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "nosckslightsabers";
+    public static final String MOD_ID = "nosckslightsabersmod";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public NoscksLightsabersMod(FMLJavaModLoadingContext context)
     {
         IEventBus modEventBus = context.getModEventBus();
+
+        ModCreativeModTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
@@ -39,9 +45,10 @@ public class NoscksLightsabersMod
     }
 
     // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event)
-    {
-
+    private void addCreative(BuildCreativeModeTabContentsEvent event)  {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.KYBERCRYSTAL);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
